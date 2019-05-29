0d4bc11b176ea307642be8851edd3cea005c4202
johbarca <330492740@qq.com>
2019-05-29 21:53:36 +0800

    添加antd，react-router等module

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>欢迎</h2>
+        </div>
+        <p className="App-intro">
+          你可以在 <code>src/App.js</code> 文件中修改。
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <Button type="primary">Primary</Button>
+      </div>
 
+    );
+  }
+}
+ 
 export default App;
+
